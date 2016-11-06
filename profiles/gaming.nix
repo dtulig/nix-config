533802ec34f424db5b9b95c7def0cbe84981f13e
David Tulig <david.tulig@gmail.com>
2016-11-06 05:49:09 -0600

    Remove dwarf-fortress, add steam and minecraft.

--- profiles/gaming.nix
+++ profiles/gaming.nix
@@ -1,9 +1,19 @@
 { config, pkgs, ... }:
-
+let
+    newSteam = pkgs.steam.override { newStdcpp = true; };
+in
 {
   environment.systemPackages = with pkgs; [
-    dwarf_fortress
-    dwarf-therapist
-    dfhack
+    #dwarf_fortress
+    #dwarf-therapist
+    #dfhack
+
+    newSteam
+
+    minecraft
   ];
+
+  fonts.enableCoreFonts = true;
+
+
 }
