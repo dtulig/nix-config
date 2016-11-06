d5752d05425af99096741d785a6f47435d970c40
David Tulig <david.tulig@gmail.com>
2016-11-06 05:57:45 -0600

    Update athens with raid setup.

--- machines/athens.nix
+++ machines/athens.nix
@@ -27,6 +27,50 @@
   # Define on which hard drive you want to install Grub.
   boot.loader.grub.device = "/dev/sda";
 
+  boot.initrd.mdadmConf = ''
+    ARRAY /dev/md0 UUID=912385cd:c0a1b495:b752d81b:fa0837fe
+  '';
+
+  boot.kernelModules = [ "dm_mod" "dm_crypt" "raid1" "aes" "sha256"
+"cbc" "xts" ];
+
+  boot.initrd.luks.cryptoModules = ["aes" "sha1" "sha256" "dm_crypt"
+"cbc" "xts" ];
+
+  boot.initrd.luks.devices = [
+   { name = "luksroot";
+      device = "/dev/md0";
+      preLVM = true;
+   }
+   { name = "lukshome"; device = "/dev/sda2"; }
+  ];
+
+  fileSystems."/" = {
+    mountPoint = "/";
+    device = "/dev/mapper/vg-system";
+  };
+
+  fileSystems."/boot" = {
+    mountPoint = "/boot";
+    device = "/dev/sda1";
+  };
+
+  fileSystems."/home" = {
+    mountPoint = "/home";
+    device = "/dev/mapper/lukshome";
+  };
+
+  fileSystems."/data" = {
+    mountPoint = "/data";
+    device = "/dev/mapper/vg-data";
+  };
+
+  swapDevices = [ {
+      device = "/var/swapfile";
+      size = 16384;
+    }
+  ];
+
   boot.loader.grub.extraEntries = ''
     menuentry "Windows 7" {
       chainloader (hd0,3)+1
@@ -45,19 +89,19 @@
   fileSystems."/mnt/Storage" = {
     device = "192.168.0.60:/c/Storage";
     fsType = "nfs";
-    options = "nolock";
+    options = ["nolock"];
   };
 
   fileSystems."/mnt/backup" = {
     device = "192.168.0.60:/c/backup";
     fsType = "nfs";
-    options = "nolock";
+    options = ["nolock"];
   };
 
   fileSystems."/mnt/media" = {
     device = "192.168.0.60:/c/media";
     fsType = "nfs";
-    options = "nolock";
+    options = ["nolock"];
   };
 
   services.duplicity = {
