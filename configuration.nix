5d80e4b648d48360439ef58d3108aba10f6f9324
David Tulig <david.tulig@gmail.com>
2017-05-08 07:57:20 -0500

    Use sandbox for builds, disable widevine for chrome.

--- configuration.nix
+++ configuration.nix
@@ -22,7 +22,7 @@ rec {
     build-fallback = true
   '';
 
-  nix.useChroot = true;
+  nix.useSandbox = true;
 
   boot.kernelModules = [ "ecryptfs" ];
   boot.cleanTmpDir = true;
@@ -61,8 +61,11 @@ rec {
     chromium = {
       enablePepperFlash = true;
       enablePepperPDF = true;
-      enableWideVine = true;
+      enableWideVine = false;
     };
+    #stdenv.userHook = ''
+    #  NIX_CFLAGS_COMPILE+=" -march=nehalem -mmmx -mno-3dnow -msse -msse2 -msse3 -mssse3 -mno-sse4a -mcx16 -msahf -mno-movbe -mno-aes -mno-sha -mno-pclmul -mpopcnt -mno-abm -mno-lwp -mno-fma -mno-fma4 -mno-xop -mno-bmi -mno-bmi2 -mno-tbm -mno-avx -mno-avx2 -msse4.2 -msse4.1 -mno-lzcnt -mno-rtm -mno-hle -mno-rdrnd -mno-f16c -mno-fsgsbase -mno-rdseed -mno-prfchw -mno-adx -mfxsr -mno-xsave -mno-xsaveopt -mno-avx512f -mno-avx512er -mno-avx512cd -mno-avx512pf -mno-prefetchwt1 -mno-clflushopt -mno-xsavec -mno-xsaves -mno-avx512dq -mno-avx512bw -mno-avx512vl -mno-avx512ifma -mno-avx512vbmi -mno-clwb -mno-pcommit -mno-mwaitx --param l1-cache-size=32 --param l1-cache-line-size=64 --param l2-cache-size=8192 -mtune=nehalem"
+    #'';
   };
 
   nixpkgs.config.packageOverrides = self: rec {
